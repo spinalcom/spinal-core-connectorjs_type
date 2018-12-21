3a54be71be9467352fc2c96b271c5f38267c0247
laurent <htd.laurent@gmail.com>
2018-12-21 12:43:37 +0100

    [fix] TypedArray.set_val def

--- src/connectorTS.ts
+++ src/connectorTS.ts
@@ -473,7 +473,7 @@ declare namespace spinal {
   class TypedArray<T> extends Model {
     dim(): number;
     size(d?: number): number;
-    set_val(index: number, value: any): void;
+    set_val(index: number| number[], value: any): void;
     nb_items(): number;
     equals(obj: any): boolean;
     resize(new_size: number| number[]): void;
