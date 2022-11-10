8c7bb997eeea713ea617b310c9e996997006fd59
Laurent HA TAM DAN <l.hatamdan@spinalcom.com>
2022-11-10 15:54:28 +0100

    fix broken export from old connector support

--- dist/SpinalModel.js
+++ dist/SpinalModel.js
@@ -37,59 +37,73 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.FileSystem = exports.BindProcess = exports.Process = exports.TypedArray_Float64 = exports.TypedArray_Int32 = exports.TypedArray = exports.Choice = exports.Ptr = exports.File = exports.Path = exports.Vec = exports.Directory = exports.Lst = exports.Val = exports.Bool = exports.Str = exports.Model = exports.spinalCore = exports.Pbr = void 0;
 __exportStar(require("spinal-core-connectorjs"), exports);
-const spinal_core_connectorjs_1 = require("spinal-core-connectorjs");
-Object.defineProperty(exports, "spinalCore", { enumerable: true, get: function () { return spinal_core_connectorjs_1.spinalCore; } });
-exports.default = spinal_core_connectorjs_1.spinalCore;
-// following is old spinalcorejs compatibility
-const spinal_core_connectorjs_2 = require("spinal-core-connectorjs");
-const Model = spinal_core_connectorjs_2.Model ? spinal_core_connectorjs_2.Model : spinal_core_connectorjs_1.spinalCore._def.Model;
-exports.Model = Model;
-const Str = spinal_core_connectorjs_2.Str ? spinal_core_connectorjs_2.Str : spinal_core_connectorjs_1.spinalCore._def.Str;
-exports.Str = Str;
-const Bool = spinal_core_connectorjs_2.Bool ? spinal_core_connectorjs_2.Bool : spinal_core_connectorjs_1.spinalCore._def.Bool;
-exports.Bool = Bool;
-const Val = spinal_core_connectorjs_2.Val ? spinal_core_connectorjs_2.Val : spinal_core_connectorjs_1.spinalCore._def.Val;
-exports.Val = Val;
-const Lst = spinal_core_connectorjs_2.Lst ? spinal_core_connectorjs_2.Lst : spinal_core_connectorjs_1.spinalCore._def.Lst;
-exports.Lst = Lst;
-const Directory = spinal_core_connectorjs_2.Directory
-    ? spinal_core_connectorjs_2.Directory
-    : spinal_core_connectorjs_1.spinalCore._def.Directory;
-exports.Directory = Directory;
-const Vec = spinal_core_connectorjs_2.Vec ? spinal_core_connectorjs_2.Vec : spinal_core_connectorjs_1.spinalCore._def.Vec;
-exports.Vec = Vec;
-const Path = spinal_core_connectorjs_2.Path ? spinal_core_connectorjs_2.Path : spinal_core_connectorjs_1.spinalCore._def.Path;
-exports.Path = Path;
-const File = spinal_core_connectorjs_2.File ? spinal_core_connectorjs_2.File : spinal_core_connectorjs_1.spinalCore._def.File;
-exports.File = File;
-const Ptr = spinal_core_connectorjs_2.Ptr ? spinal_core_connectorjs_2.Ptr : spinal_core_connectorjs_1.spinalCore._def.Ptr;
-exports.Ptr = Ptr;
-const Pbr = spinal_core_connectorjs_2.Pbr ? spinal_core_connectorjs_2.Pbr : spinal_core_connectorjs_1.spinalCore._def.Pbr;
-exports.Pbr = Pbr;
-const Choice = spinal_core_connectorjs_2.Choice ? spinal_core_connectorjs_2.Choice : spinal_core_connectorjs_1.spinalCore._def.Choice;
-exports.Choice = Choice;
-const TypedArray = spinal_core_connectorjs_2.TypedArray
-    ? spinal_core_connectorjs_2.TypedArray
-    : spinal_core_connectorjs_1.spinalCore._def.TypedArray;
-exports.TypedArray = TypedArray;
-const TypedArray_Int32 = spinal_core_connectorjs_2.TypedArray_Int32
-    ? spinal_core_connectorjs_2.TypedArray_Int32
-    : spinal_core_connectorjs_1.spinalCore._def.TypedArray_Int32;
-exports.TypedArray_Int32 = TypedArray_Int32;
-const TypedArray_Float64 = spinal_core_connectorjs_2.TypedArray_Float64
-    ? spinal_core_connectorjs_2.TypedArray_Float64
-    : spinal_core_connectorjs_1.spinalCore._def.TypedArray_Float64;
-exports.TypedArray_Float64 = TypedArray_Float64;
-const Process = spinal_core_connectorjs_2.Process ? spinal_core_connectorjs_2.Process : spinal_core_connectorjs_1.spinalCore._def.Process;
-exports.Process = Process;
-const BindProcess = spinal_core_connectorjs_2.BindProcess
-    ? spinal_core_connectorjs_2.BindProcess
-    : spinal_core_connectorjs_1.spinalCore._def.BindProcess;
-exports.BindProcess = BindProcess;
-const FileSystem = spinal_core_connectorjs_2.FileSystem
-    ? spinal_core_connectorjs_2.FileSystem
-    : spinal_core_connectorjs_1.spinalCore._def.FileSystem;
-exports.FileSystem = FileSystem;
+const spinalCore = require('spinal-core-connectorjs');
+exports.default = spinalCore;
+// // following is old spinalcorejs compatibility
+if (!Object.prototype.hasOwnProperty.call(exports, 'spinalCore')) {
+    Object.defineProperty(exports, 'spinalCore', {
+        enumerable: true,
+        get: function () {
+            // @ts-ignore
+            return spinalCore;
+        },
+    });
+}
+if (!Object.prototype.hasOwnProperty.call(exports, 'FileSystem')) {
+    Object.defineProperty(exports, 'FileSystem', {
+        enumerable: true,
+        get: function () {
+            // @ts-ignore
+            return FileSystem;
+        },
+    });
+}
+if (!Object.prototype.hasOwnProperty.call(exports, 'ModelProcessManager')) {
+    Object.defineProperty(exports, 'ModelProcessManager', {
+        enumerable: true,
+        get: function () {
+            // @ts-ignore
+            return ModelProcessManager;
+        },
+    });
+}
+if (!Object.prototype.hasOwnProperty.call(exports, 'SpinalUserManager')) {
+    Object.defineProperty(exports, 'SpinalUserManager', {
+        enumerable: true,
+        get: function () {
+            // @ts-ignore
+            return SpinalUserManager;
+        },
+    });
+}
+for (const key of [
+    'Model',
+    'Str',
+    'Bool',
+    'Val',
+    'Lst',
+    'Directory',
+    'Vec',
+    'Path',
+    'File',
+    'Ptr',
+    'Pbr',
+    'Choice',
+    'TypedArray',
+    'TypedArray_Int32',
+    'TypedArray_Float64',
+    'Process',
+    'BindProcess',
+]) {
+    if (!Object.prototype.hasOwnProperty.call(exports, key)) {
+        Object.defineProperty(exports, key, {
+            enumerable: true,
+            get: function () {
+                // @ts-ignore
+                return _spinalCore._def[key];
+            },
+        });
+    }
+}
 //# sourceMappingURL=SpinalModel.js.map
--- src/SpinalModel.ts
+++ src/SpinalModel.ts
@@ -23,90 +23,73 @@
  */
 
 export * from 'spinal-core-connectorjs';
-import { spinalCore } from 'spinal-core-connectorjs';
-export default spinalCore;
+const spinalCore = require('spinal-core-connectorjs');
+import type { spinalCore as _spinalCore } from 'spinal-core-connectorjs';
+export default <typeof _spinalCore>spinalCore;
 
-// following is old spinalcorejs compatibility
-
-import {
-  Model as _Model,
-  Str as _Str,
-  Bool as _Bool,
-  Val as _Val,
-  Lst as _Lst,
-  Directory as _Directory,
-  Vec as _Vec,
-  Path as _Path,
-  File as _File,
-  Ptr as _Ptr,
-  Choice as _Choice,
-  TypedArray as _TypedArray,
-  TypedArray_Int32 as _TypedArray_Int32,
-  TypedArray_Float64 as _TypedArray_Float64,
-  Process as _Process,
-  BindProcess as _BindProcess,
-  FileSystem as _FileSystem,
-  Pbr as _Pbr,
-} from 'spinal-core-connectorjs';
-
-const Model: typeof _Model = _Model ? _Model : spinalCore._def.Model;
-
-const Str: typeof _Str = _Str ? _Str : spinalCore._def.Str;
-
-const Bool: typeof _Bool = _Bool ? _Bool : spinalCore._def.Bool;
-
-const Val: typeof _Val = _Val ? _Val : spinalCore._def.Val;
-
-const Lst: typeof _Lst = _Lst ? _Lst : spinalCore._def.Lst;
-const Directory: typeof _Directory = _Directory
-  ? _Directory
-  : spinalCore._def.Directory;
-
-const Vec: typeof _Vec = _Vec ? _Vec : spinalCore._def.Vec;
-
-const Path: typeof _Path = _Path ? _Path : spinalCore._def.Path;
-
-const File: typeof _File = _File ? _File : spinalCore._def.File;
-
-const Ptr: typeof _Ptr = _Ptr ? _Ptr : spinalCore._def.Ptr;
-
-const Pbr: typeof _Pbr = _Pbr ? _Pbr : spinalCore._def.Pbr;
-
-const Choice: typeof _Choice = _Choice ? _Choice : spinalCore._def.Choice;
-const TypedArray: typeof _TypedArray = _TypedArray
-  ? _TypedArray
-  : spinalCore._def.TypedArray;
-const TypedArray_Int32: typeof _TypedArray_Int32 = _TypedArray_Int32
-  ? _TypedArray_Int32
-  : spinalCore._def.TypedArray_Int32;
-const TypedArray_Float64: typeof _TypedArray_Float64 = _TypedArray_Float64
-  ? _TypedArray_Float64
-  : spinalCore._def.TypedArray_Float64;
-
-const Process: typeof _Process = _Process ? _Process : spinalCore._def.Process;
-const BindProcess: typeof _BindProcess = _BindProcess
-  ? _BindProcess
-  : spinalCore._def.BindProcess;
-const FileSystem: typeof _FileSystem = _FileSystem
-  ? _FileSystem
-  : spinalCore._def.FileSystem;
-
-export { Pbr };
-export { spinalCore };
-export { Model };
-export { Str };
-export { Bool };
-export { Val };
-export { Lst };
-export { Directory };
-export { Vec };
-export { Path };
-export { File };
-export { Ptr };
-export { Choice };
-export { TypedArray };
-export { TypedArray_Int32 };
-export { TypedArray_Float64 };
-export { Process };
-export { BindProcess };
-export { FileSystem };
+// // following is old spinalcorejs compatibility
+if (!Object.prototype.hasOwnProperty.call(exports, 'spinalCore')) {
+  Object.defineProperty(exports, 'spinalCore', {
+    enumerable: true,
+    get: function () {
+      // @ts-ignore
+      return spinalCore;
+    },
+  });
+}
+if (!Object.prototype.hasOwnProperty.call(exports, 'FileSystem')) {
+  Object.defineProperty(exports, 'FileSystem', {
+    enumerable: true,
+    get: function () {
+      // @ts-ignore
+      return FileSystem;
+    },
+  });
+}
+if (!Object.prototype.hasOwnProperty.call(exports, 'ModelProcessManager')) {
+  Object.defineProperty(exports, 'ModelProcessManager', {
+    enumerable: true,
+    get: function () {
+      // @ts-ignore
+      return ModelProcessManager;
+    },
+  });
+}
+if (!Object.prototype.hasOwnProperty.call(exports, 'SpinalUserManager')) {
+  Object.defineProperty(exports, 'SpinalUserManager', {
+    enumerable: true,
+    get: function () {
+      // @ts-ignore
+      return SpinalUserManager;
+    },
+  });
+}
+for (const key of [
+  'Model',
+  'Str',
+  'Bool',
+  'Val',
+  'Lst',
+  'Directory',
+  'Vec',
+  'Path',
+  'File',
+  'Ptr',
+  'Pbr',
+  'Choice',
+  'TypedArray',
+  'TypedArray_Int32',
+  'TypedArray_Float64',
+  'Process',
+  'BindProcess',
+]) {
+  if (!Object.prototype.hasOwnProperty.call(exports, key)) {
+    Object.defineProperty(exports, key, {
+      enumerable: true,
+      get: function () {
+        // @ts-ignore
+        return _spinalCore._def[key];
+      },
+    });
+  }
+}
